b1e7831a52d0467ca42d7da415a123363bb57ec9
anytimemusic <bima220711@gmail.com>
2026-05-28 10:34:59 +0700

    pengecekan antrian kosong

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -48,4 +48,10 @@ public:
 
     void remove()
     {
+        // cek apakah antrian kosong
+        if (FRONT == -1)
+        {
+            cout << "Queue underflow\n";
+            return;
+        }
        
